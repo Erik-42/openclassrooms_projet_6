edde7357ae4dd7a62eca993102d72c7a96219d84
Erik-42 <mesen.erik@gmail.com>
2023-06-08 20:51:59 +0200

    CommitSCSS2

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Accueil from './Pages/Accueil/Accueil.jsx';
 import Logement from './Pages/Logement/Logement.jsx';
 import Apropos from './Pages/Apropos/Apropos.jsx';
 import Error404 from './Pages/Error404/Error404.jsx';
-import styled from './Style/App.module.scss';
+import './Style/App.scss';
 
 function App() {
   return (
-    <div className={styled.App}>
+    <div className='app'>
       <BrowserRouter>
         <Header />
         <Routes>
